de3743854946171a69978887b9422c4f1d19eff3
voronov <git@eyeline.mobi>
2017-07-27 22:02:31 +0700

    #6: added test mode (some test data initialization) + updated readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # subscriber-list-service
 REST service for processing subscriber lists. Apache Ignite, JAX-RS, Docker.
+
+1. Add association Cell - Phone:
+POST /rest/msisdn
+{
+"cellId"	:	1, 
+"ctn"     :	12345678
+}
+
+2. Add profile:
+POST /rest/profile
+{
+"ctn"			      :	12345678,
+"name"			    :	"Willson",
+"email"			    :	"willson@example.com",
+"activateDate"	:	"2017-02-24 12:45:00"
+}
+
+3. Get subscribers:
+GET /rest/subscribers?cellId=1
+
--- service/src/org/unknown/services/subscriber/SubscriberService.java
+++ service/src/org/unknown/services/subscriber/SubscriberService.java
@@ -32,6 +32,11 @@ public class SubscriberService {
     msisdns = db.getOrCreateCache("MSISDNS", Integer.class, Msisdn.class);
     collocated = db.getOrCreateCache("COLLOCATED_CELLS_AND_MSISDNS", AffinityKey.class, Msisdn.class);
     profiles = db.getOrCreateCache("PROFILES");
+
+    boolean testMode = System.getProperty("test.mode") != null ? Boolean.valueOf(System.getProperty("test.mode")) : false;
+
+    if (testMode)
+      initTestData();
   }
 
   public void createMsisdn(Msisdn msisdn) {
@@ -56,5 +61,28 @@ public class SubscriberService {
 
   public Profile getProfile(Integer ctn) {
     return db.tx(() -> profiles.get(ctn));
+  }
+
+  private void initTestData() {
+    Profile profile1 = new Profile("Bob", "bob@example.com", "2016-12-11 15:05:01");
+    Profile profile2 = new Profile("Tom", "tom@example.com", "2016-12-12 16:05:01");
+    Profile profile3 = new Profile("Hank", "hank@example.com", "2018-07-27 11:05:01");
+
+    Integer ctn1 = 111111111;
+    Integer ctn2 = 222222222;
+    Integer ctn3 = 333333333;
+
+    Msisdn msisdn1 = new Msisdn(ctn1, 1);
+    Msisdn msisdn2 = new Msisdn(ctn2, 1);
+    Msisdn msisdn3 = new Msisdn(ctn3, 2);
+
+    createProfile(ctn1, profile1);
+    createProfile(ctn2, profile2);
+    createProfile(ctn3, profile3);
+
+    createMsisdn(msisdn1);
+    createMsisdn(msisdn2);
+    createMsisdn(msisdn3);
+
   }
 }
